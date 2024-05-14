0a22a95f590dc32c6fb13666553424769a3ae42a
sneha34258 <160130172+sneha34258@users.noreply.github.com>
2024-05-15 03:14:55 +0530

    Add files via upload

---
+++ arrays/intro.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+    // int marks1 = 97;
+    // int marks2 = 99;
+    // int marks3 = 89;
+
+    int marks[3];
+    printf("enter phy : ");
+    scanf("%d", &marks[0]);
+
+    printf("enter chem : ");
+    scanf("%d", &marks[1]);
+
+    printf("enter math : ");
+    scanf("%d", &marks[2]);
+
+    printf("phy = %d, chem=%d, math = %d",marks[0], marks[1],marks[2]);
+    
+
+
+    
+
+  
+   return 0;
+ }
