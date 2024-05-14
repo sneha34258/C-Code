7bd368b81fb684aea0afa6f44a988451bfbdf878
sneha34258 <160130172+sneha34258@users.noreply.github.com>
2024-05-15 03:16:04 +0530

    Add files via upload

---
+++ 2Darrays/qwes2.c
@@ -0,0 +1,20 @@
+//write a program to store first n fibonachi no.
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("enter n (n>2): ");
+    scanf("%d", &n);
+
+    int fib[n];
+    fib[0]=0;
+    fib[1]=1;
+
+    for(int i=2; i<n;i++){
+        fib[i] = fib[i-1 ] + fib[i-2];
+        printf("%d\t",fib[i]);
+    }
+
+  
+   return 0;
+ }
